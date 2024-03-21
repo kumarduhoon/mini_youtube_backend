c7197e6937104b764b1c39a652fd8e8a8d1fc7ad
Akash Kumar <akash.kumar@arkasoftwares.com>
2024-03-21 12:08:14 +0530

    access and refresh token

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express"
-import { registerUser, loginUser, logoutUser } from "../controllers/user.controller.js";
+import { registerUser, loginUser, logoutUser, refreshAccessToken } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js"
 import { verifiedUserWithJWT } from "../middlewares/auth.middleware.js"
 
@@ -18,6 +18,7 @@ router.route("/register").post(
     registerUser)
 
 router.route("/login").post(loginUser)
+router.route("/refresh-token").post(refreshAccessToken)
 
 // protected route 
 
